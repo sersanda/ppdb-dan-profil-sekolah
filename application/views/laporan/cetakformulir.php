8103987f9d6ddd858fef6698eb9dc1d931f8f7e2
sersanda <156287952+sersanda@users.noreply.github.com>
2024-02-28 11:29:08 +0000

    Pending changes exported from your codespace

---
+++ application/views/laporan/cetakformulir.php
@@ -0,0 +1,456 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+	<title>
+		<?= $web['nama'] ?>
+	</title>
+	<meta content="<?= $web['deskripsi'] ?>" name="description">
+	<meta content="" name="keywords">
+	<!-- Favicons -->
+	<link href="<?= base_url('assets/'); ?>img/<?= $web['fav'] ?>" rel="icon">
+	<link href="<?= base_url('assets/'); ?>img/<?= $web['fav'] ?>" rel="apple-touch-icon">
+	<!-- Google Fonts -->
+	<link
+		href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+		rel="stylesheet">
+	<!-- Vendor CSS Files -->
+	<link href="<?= base_url('assets/'); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?= base_url('assets/'); ?>vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="<?= base_url('assets/'); ?>vendor/aos/aos.css" rel="stylesheet">
+	<link href="<?= base_url('assets/'); ?>vendor/remixicon/remixicon.css" rel="stylesheet">
+	<link href="<?= base_url('assets/'); ?>vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+	<link href="<?= base_url('assets/'); ?>vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<!-- Template Main CSS File -->
+	<link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
+	<!-- Select2 CSS -->
+	<link href="<?= base_url('assets/') ?>select2/dist/css/select2.min.css" rel="stylesheet" />
+	<!-- jQuery library -->
+	<script src="<?= base_url('assets/') ?>js/jquery-3.3.1.min.js"></script>
+
+	<style type="text/css">
+		html {
+			font-size: 10px;
+			-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+		}
+
+		body {
+			font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+			font-size: 12px;
+			line-height: 1.42857143;
+			color: #000;
+			background-color: #fff;
+		}
+
+		table {
+			border-spacing: 0;
+			border-collapse: collapse
+		}
+
+		td,
+		th {
+			padding: 0;
+		}
+
+		.text-left {
+			text-align: left;
+		}
+
+		.text-right {
+			text-align: right;
+		}
+
+		.text-center {
+			text-align: center;
+		}
+
+		.text-justify {
+			text-align: justify;
+		}
+
+		.text-nowrap {
+			white-space: nowrap;
+		}
+
+		.text-lowercase {
+			text-transform: lowercase;
+		}
+
+		.text-uppercase {
+			text-transform: uppercase;
+		}
+
+		.text-capitalize {
+			text-transform: capitalize;
+		}
+
+		.pull-right {
+			float: right !important;
+		}
+
+		.pull-left {
+			float: left !important;
+		}
+
+		.kiri {
+			padding-left: 4px;
+		}
+
+		.kanan {
+			padding-right: 4px;
+		}
+
+		.btop {
+			border-top: 1px solid black;
+		}
+
+		.bbottom {
+			border-bottom: 1px solid black;
+		}
+
+		.bleft {
+			border-left: 1px solid black;
+		}
+
+		.bright {
+			border-right: 1px solid black;
+		}
+
+		.ball {
+			border: 1px solid black;
+		}
+
+		.container {
+			position: absolute;
+			top: 25%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+
+		.box {
+			border: 1px solid black;
+			padding: 10px;
+		}
+
+		.container1 {
+			position: absolute;
+			top: 78%;
+			left: 1%;
+
+		}
+
+		.box1 {
+			border: 1px solid black;
+			padding: 10px;
+		}
+
+		.double-line {
+			border-top: 3px double #000;
+		}
+	</style>
+</head>
+
+<body>
+<table width="84%" border="0" cellpadding="3" cellspacing="0">
+		<tbody>
+			<tr>
+				<td width="1%" align="left" valign="top">
+					<img src="assets/img/logo_sekolah.png" width="105px" height="130px">
+				</td>
+				<td width="79%" valign="top" align="center" valign="center">
+					<span style="font-weight: bold; font-size: 20px">Majelis Pendidikan Dasar dan Menengah</span><br>
+					<span style="font-weight: bold; font-size: 20px">Pimpinan Daerah Muhammadiyah
+						Tanjungpinang</span><br>
+					<span style="font-weight: bold; font-size: 20px">
+						<?= $web['nama'] ?>
+					</span><br>
+					<span style="font-weight: bold; font-size: 18px;">NSS : 202316102015 </span><span
+						style="font-weight: bold; font-size: 18px;">- TERAKREDITASI "C" </span><br>
+					<span style="font-size: 12px;">
+						Jl. Raja Haji Fisabililllah No. 70 KM 8 , Kota Tanjungpinang , Prov. Kepulauan Riau, 29464
+					</span><br>
+					<span style="font-size: 12px;">
+						Telp : (0771) - 4500056&sdot; | Email : smp-muhammadiyahtpi@gmail.co.id</p>
+					</span>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<hr class="double-line">
+	<p class="text-center">
+		<span style="font-weight: bold; font-size: 16px">FORM PENDAFTARAN ULANG</span>
+		<br>
+		<span style="font-weight: bold; font-size: 16px">PPDB T.A
+			<?= date("Y"); ?> / <?= date("Y") + 1; ?>
+		</span>
+		<br>
+	<br><br>
+	</p><br><br><br><br>
+	
+	
+	
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+
+	<!-- halaman 2 -->
+	<table width="84%" border="0" cellpadding="3" cellspacing="0">
+		<tbody>
+			<tr>
+				<td width="1%" align="left" valign="top">
+					<img src="assets/img/logo_sekolah.png" width="105px" height="130px">
+				</td>
+				<td width="79%" valign="top" align="center" valign="center">
+					<span style="font-weight: bold; font-size: 20px">Majelis Pendidikan Dasar dan Menengah</span><br>
+					<span style="font-weight: bold; font-size: 20px">Pimpinan Daerah Muhammadiyah
+						Tanjungpinang</span><br>
+					<span style="font-weight: bold; font-size: 20px">
+						<?= $web['nama'] ?>
+					</span><br>
+					<span style="font-weight: bold; font-size: 18px;">NSS : 202316102015 </span><span
+						style="font-weight: bold; font-size: 18px;">- TERAKREDITASI "C" </span><br>
+					<span style="font-size: 12px;">
+						Jl. Raja Haji Fisabililllah No. 70 KM 8 , Kota Tanjungpinang , Prov. Kepulauan Riau, 29464
+					</span><br>
+					<span style="font-size: 12px;">
+						Telp : (0771) - 4500056&sdot; | Email : smp-muhammadiyahtpi@gmail.co.id</p>
+					</span>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<hr class="double-line">
+
+	<p class="text-center">
+		<span style="font-weight: bold; font-size: 16px">FORMULIR PENDAFTARAN SISWA BARU</span>
+		<br>
+		<span style="font-weight: bold; font-size: 16px">TAHUN PELAJARAN
+			<?= date("Y"); ?>/
+			<?= date("Y") + 1; ?>
+		</span>
+		<br>
+	<div class="container">
+		<div class="box">
+			<span style="font-weight: bold; font-size: 14px">No. Pendaftaran :
+				<?= $ppdb['no_daftar'] ?>
+			</span>
+		</div>
+	</div><br><br>
+	</p><br><br>
+	<h2><b>A. Biodata siswa</b></h2>
+	<table class="text-justify" width="100%" style="font-size: 14px;" cellspacing="1">
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Nama Lengkap</td>
+			<td>:</td>
+			<td>
+				<?= strtoupper($ppdb['nama']); ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Tempat / Tanggal Lahir</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['kab'] ?> / <?= longdate_indo(date($ppdb['ttl'])) ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Jenis Kelamin</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['jk'] ?>
+			</td>
+		</tr>
+		<!-- <tr>
+			<td align="" style="color: white;">. </td>
+			<td>NISN</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nis'] ?>
+			</td>
+		</tr> -->
+		
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Alamat</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['alamat'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Asal Sekolah</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['sekolah_asal'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Email</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['email'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>No HP</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['no_hp'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Nama Ayah</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nama_ayah'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Nama Ibu</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nama_ibu'] ?>
+			</td>
+		</tr>
+	</table><br>
+	<h2><b>B. Data Orang Tua / Wali</b></h2>
+	<table class="text-justify" width="100%" style="font-size: 14px;" cellspacing="1">
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Nama Lengkap</td>
+			<td>:</td>
+			<td>
+				<?= strtoupper($ppdb['nama']); ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>NIK</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nik'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>NISN</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nis'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Jenis Kelamin</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['jk'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Tempat Lahir</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['kab'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Tanggal Lahir</td>
+			<td>:</td>
+			<td>
+				<?= mediumdate_indo(date($ppdb['ttl'])) ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Alamat</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['alamat'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Asal Sekolah</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['sekolah_asal'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Email</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['email'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>No HP</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['no_hp'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Nama Ayah</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nama_ayah'] ?>
+			</td>
+		</tr>
+		<tr>
+			<td align="" style="color: white;">. </td>
+			<td>Nama Ibu</td>
+			<td>:</td>
+			<td>
+				<?= $ppdb['nama_ibu'] ?>
+			</td>
+		</tr>
+	</table><br>
+	<br>
+	<br>
+	<br>
+	<br>
+	
+	<!-- <table width="100%" style="font-size: 14px;">
+		<tr>
+			<td width="50%" align="center" style="color: white;">.</td>
+			<td width="50%" align="center" style="color: white;">.</td>
+			<td width="70%" align="center">Tanjungpinang, <?= longdate_indo(date('Y-m-d')); ?>
+			</td>
+		</tr>
+		<tr>
+
+			<td width="50%" align="center">Wali Murid<br><br>
+				<div class="container1">
+					<div class="box1">
+						<span style="font-weight: bold; font-size: 10px">Rp. 10.000
+						</span>
+					</div>
+				</div>
+				<br><br><br><br>(
+				................................... )
+			</td>
+			<td width="50%" align="center" style="color: white;">.</td>
+			<td width="50%" align="center">Panitia PPDB SMP Muhammadiyah Tanjungpinang<br><br><br><br><br>(
+				................................... )</td>
+		</tr>
+	</table> -->
+
+</body>
